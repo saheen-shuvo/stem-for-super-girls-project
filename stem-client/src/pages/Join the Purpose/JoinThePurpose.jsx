a9ea6376471afa7e27008067f0163b402c1dc3b0
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2026-08-04 22:41:54 +0600

    Build Fixed

--- stem-client/src/pages/Join the Purpose/JoinThePurpose.jsx
+++ stem-client/src/pages/Join the Purpose/JoinThePurpose.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import JpBanner from './Join The Purpose Components/jpBanner';
+import JpBanner from './Join The Purpose Components/JpBanner';
 import ThreeCards from './Join The Purpose Components/ThreeCards';
 import Partners from './Join The Purpose Components/Partners';
 
